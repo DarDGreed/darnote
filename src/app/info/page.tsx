fd97f40d166c5b7ea2e8447172b0b2b4f57cf1c6
ShadyDar <141896824+DarDGreed@users.noreply.github.com>
2025-04-02 15:15:21 +0800

    Update page.tsx

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -2,6 +2,8 @@ export default function ProjectInfo() {
   return(
     <div className="text-black flex flex-col p-10 items-center justify-center">
       <p>This Project was made while learning Nextjs with MongoDB</p>
+      <p>^_^</p>
+      
     </div>
   )
 }
