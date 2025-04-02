0bcbca339700732dbe8119f31b0afb204fbf2b31
ShadyDar <141896824+DarDGreed@users.noreply.github.com>
2025-04-02 15:27:53 +0800

    Update ItemsList.tsx

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -4,47 +4,48 @@ import { FilePenLine, Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-interface Item{
+interface Item {
   _id: string;
   name: string;
   content: string;
 }
 
-export default function ItemsList(){
-  const [items, setItems] = useState([])
+export default function ItemsList() {
+  const [items, setItems] = useState<Item[]>([]);
   const router = useRouter();
+
   useEffect(() => {
-    fetchItems()
-  }, [])
+    fetchItems();
+  }, []);
 
   const fetchItems = async () => {
-    try{
-    const response = await axios.get("/api")
-    setItems(response.data.items)
-    }catch(error: any){
-      console.error("error fetching items", error.message)
-    }
-  }
-
-  const handleDelete = (itemId: any) => {
-    console.log(itemId)
     try {
-      axios.delete(`/api/${itemId}`)
-      setItems((prevItems) => prevItems.filter((item) => item._id !== itemId))
+      const response = await axios.get("/api");
+      setItems(response.data.items);
     } catch (error: any) {
-      console.error("error deleting item", error.message)
+      console.error("error fetching items", error.message);
     }
-  }
-  const handleUpdate = (itemId: any) => {
-    console.log(itemId)
-    router.push(`/?show=true/${itemId}`)
-  }
+  };
 
+  const handleDelete = async (itemId: string) => {
+    console.log(itemId);
+    try {
+      await axios.delete(`/api/${itemId}`);
+      setItems((prevItems) => prevItems.filter((item) => item._id !== itemId));
+    } catch (error: any) {
+      console.error("error deleting item", error.message);
+    }
+  };
 
-  return(
+  const handleUpdate = (itemId: string) => {
+    console.log(itemId);
+    router.push(`/?show=true/${itemId}`);
+  };
+
+  return (
     <>
-      {items.map((item: Item, index) => (
-        <div key={index}>
+      {items.map((item) => (
+        <div key={item._id}>
           <div className="bg-green-300 m-4 rounded p-4 flex justify-between items-center">
             <div>
               <div className="flex gap-2">
@@ -54,7 +55,7 @@ export default function ItemsList(){
               <p>{item.content}</p>
             </div>
             <div className="flex gap-4">
-              <button onClick={() => {handleUpdate(item._id)}} className="cursor-pointer">
+              <button onClick={() => handleUpdate(item._id)} className="cursor-pointer">
                 <FilePenLine color="black" />
               </button>
               <button onClick={() => handleDelete(item._id)} className="cursor-pointer">
@@ -62,9 +63,8 @@ export default function ItemsList(){
               </button>
             </div>
           </div>
-          
         </div>
       ))}
     </>
-  )
+  );
 }
